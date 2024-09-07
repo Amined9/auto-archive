b4f71fbe097844e0fcaa004dccecbb029bcc758c
Amined9 <mahalhal.amine.sio@gmail.com>
2024-09-07 23:42:53 +0200

    fin-sa

---
+++ archive-backend/resources/views/Components/Register.blade.php
@@ -0,0 +1,27 @@
+<div class="bg-gray-200 rounded-lg shadow-lg max-w-[700px] max-h-[900px] mx-6 xl:mx-auto mt-12">
+    <div class="text-3xl text-center text-bold p-6">
+
+        <form action="" method="post">
+            <div class="flex flex-col space-y-6 items-center">
+                <h2 class="">You are new here</h2>
+                <div class="w-4/5 text-left text-xl">
+                    <label for="name">name</label>
+                    <input type="text" name="name" id="name" class="w-full rounded-lg p-1 decoration-transparent">
+                </div>
+                <div class="w-4/5 text-left text-xl">
+                    <label for="email">email</label>
+                    <input type="email" name="email" id="email" class="w-full rounded-lg p-1 decoration-transparent">
+                </div>
+                <div class="w-4/5 text-left text-xl">
+                    <label for="pass">password</label>
+                    <input type="password" name="pass" id="pass" class="w-full rounded-lg p-1 decoration-transparent">
+                </div>
+                <div class="w-4/5">
+                    <a href="/"></a><button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 me-2 mb-2 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Register</button>
+                </div>
+            </div>
+
+        </form>
+    </div>
+
+</div>
--- archive-backend/resources/views/index.blade.php
+++ archive-backend/resources/views/index.blade.php
@@ -1,20 +1,17 @@
 @include('Layouts.header')
-<div class="h-screen flex flex-row">
-    <div class="bg-slate-50 h-screen w-1/2">
-        <img src="{{ asset(storage_path('undraw_data_extraction_re_0rd3.svg')) }}" height="500" width="500" alt="">
-    </div>
-    <div class="bg-blue-600 h-screen w-1/2">
-        <div class="h-80 w-96 bg-white rounded-lg max-w-[700px] mx-auto mt-96">
-            <div class="text-2xl text-center text-semibold p-6">
-                <span class="">Hi welcome back</span>
-            </div>
-            <div>
-
-            </div>
-
-
+<div class="md:h-screen flex flex-col md:flex-row ">
+    <div class="bg-slate-50 h-screen justify-center mx-auto md:w-1/2">
+        <div class="size-72 mx-auto">
+            <img src="{{ asset('storage/Dashboard.svg') }}" height="500px">
         </div>
+        @if (session('status'))
+            @include('Components.Register')
+        @else
+            @include('Components.Login')
+        @endif
+    </div>
+    <div class="bg-gray-200 h-screen md:w-1/2">
+        <img class="size-2/3 mx-auto" src="{{ asset('storage/logo.svg') }}" alt="image">
     </div>
 </div>
-
 @include('Layouts.footer')
