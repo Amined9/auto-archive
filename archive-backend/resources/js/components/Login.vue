7a9b066e247d0e8a490ca4782fb46b9de14677a4
Amined9 <mahalhal.amine.sio@gmail.com>
2024-09-15 00:01:33 +0200

    fin-sa

---
+++ archive-backend/resources/js/components/Login.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="bg-gray-200 rounded-lg shadow-lg max-w-[700px] max-h-[900px] mx-6 xl:mx-auto mt-12">
+    <div class="text-3xl text-center text-bold p-6">
+
+        <form action="" method="post">
+            <div class="flex flex-col space-y-6 items-center">
+                <h2 class="">Hi welcome back</h2>
+                <div class="w-4/5 text-left text-xl">
+                    <label for="email">email</label>
+                    <input type="email" name="email" id="email" class="w-full rounded-lg p-1 decoration-transparent">
+                </div>
+                <div class="w-4/5 text-left text-xl">
+                    <label for="pass">password</label>
+                    <input type="password" name="pass" id="pass" class="w-full rounded-lg p-1 decoration-transparent">
+                </div>
+                <div class="w-4/5">
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 me-2 mb-2 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Login</button>
+                </div>
+            </div>
+            <div class="w-4/5 mx-auto">
+                <div class="text-sm">Don't have an account ?</div>
+                <a href="/"></a><button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-sm text-sm px-5 py-2.5 me-2 mb-2 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Register</button>
+            </div>
+
+        </form>
+    </div>
+</div>
+</template>
+<script>
+
+
+</script>
